31edee7120666893b907a0170519d1aee005152e
Nick Kubarko <kbrn.work@gmail.com>
2025-12-01 23:05:30 +0300

    playing with wagmi, viem

---
+++ client/src/shared/config/contracts.ts
@@ -0,0 +1,6 @@
+import { raffleAbi } from '../../abi/Raffle.abi';
+
+export const raffleContract = {
+  address: '0x912860C6930FbC7c4932eA4aAB8b21C104c6d4B7',
+  abi: raffleAbi,
+} as const;
--- client/src/widgets/raffle/ui/Raffle.tsx
+++ client/src/widgets/raffle/ui/Raffle.tsx
@@ -1,68 +1,147 @@
+import { useCallback, useMemo, useState } from 'react';
+import { formatEther, parseEther } from 'viem';
 import {
-  createPublicClient,
-  createWalletClient,
-  custom,
-  getContract,
-  http,
-} from 'viem';
-import { sepolia } from 'viem/chains';
-import { useReadContract } from 'wagmi';
-import { raffleAbi } from '../../../abi/Raffle.abi';
+  useConnection,
+  useReadContract,
+  useReadContracts,
+  useWatchContractEvent,
+  useWriteContract,
+} from 'wagmi';
+import { raffleContract } from '../../../shared/config/contracts';
 
 function Raffle() {
+  const { address } = useConnection();
+  const { writeContract, isPending: pendingWrite } = useWriteContract();
+  const [bidAmount, setBidAmount] = useState<number | null>(null);
+
   const { data: raffleOwner } = useReadContract({
-    address: '0x912860C6930FbC7c4932eA4aAB8b21C104c6d4B7',
-    abi: raffleAbi,
+    ...raffleContract,
     functionName: 'i_owner',
     args: [],
   });
 
-  console.log('raffleOwner:', raffleOwner);
-
-  const publicClient = createPublicClient({
-    chain: sepolia,
-    transport: http(),
+  useWatchContractEvent({
+    ...raffleContract,
+    eventName: 'RaffleWinnerPicked',
+    enabled: true,
   });
 
-  const walletClient = createWalletClient({
-    chain: sepolia,
-    transport: custom(window.ethereum),
+  const { data: readData } = useReadContracts({
+    contracts: [
+      {
+        ...raffleContract,
+        functionName: 'getPlayerSlotsCount',
+      },
+      {
+        ...raffleContract,
+        functionName: 'getUniquePlayersCount',
+      },
+      {
+        ...raffleContract,
+        functionName: 'getFeePriceEth',
+      },
+    ],
   });
 
-  const raffleContract = getContract({
-    address: '0x912860C6930FbC7c4932eA4aAB8b21C104c6d4B7',
-    abi: raffleAbi,
-    client: {
-      public: publicClient,
-      wallet: walletClient,
+  const [playersCount, uniquePlayersCount, feePriceEthResponse] =
+    readData ?? [];
+
+  // useEffect(() => {
+  //   const res = feePriceEthResponse?.result;
+  //   const feePrice = res ? formatEther(res) : 0;
+
+  //   setBidAmount(Number(feePrice));
+  // }, [feePriceEthResponse]);
+
+  const defaultBidAmount = useMemo(() => {
+    const res = feePriceEthResponse?.result;
+    const feePrice = res ? formatEther(res) : '';
+
+    return feePrice;
+  }, [feePriceEthResponse?.result]);
+
+  const enterRaffle = useCallback(
+    (value: string) => {
+      writeContract({
+        ...raffleContract,
+        functionName: 'enter',
+        args: [],
+        value: parseEther(value),
+      });
     },
-  });
+    [writeContract],
+  );
 
-  const enterRaffle = async () => {
-    const block = await publicClient.getBlock();
-    console.log(block);
-
-    const addresses = await walletClient.getAddresses();
-    console.log(addresses);
-
-    const tx = await raffleContract.read.getFeePriceEth();
-    console.log('getFeePriceEth:', tx);
+  const pickWinner = () => {
+    writeContract({
+      ...raffleContract,
+      functionName: 'pickWinnerByOwner',
+      args: [],
+    });
   };
+
+  const requestDraw = () => {
+    console.log('requestDraw');
+  };
+
+  const changeBidAmount = (value: string) => {
+    const _decimal = Number(value);
+    if (!Number.isNaN(_decimal)) {
+      setBidAmount(_decimal);
+    }
+  };
+
+  console.log('bidAmount:', bidAmount, defaultBidAmount);
 
   return (
     <div>
       <div>
         <h4>Raffle</h4>
-        {raffleOwner && <div>Raffle owner: {raffleOwner}</div>}
+
+        <div>
+          <div>Players count: {playersCount?.result ?? 0}</div>
+          <div>Unique players count: {uniquePlayersCount?.result ?? 0}</div>
+        </div>
+
+        <hr />
+
+        {/* <div>Raffle owner: {raffleOwner ?? 'loading...'}</div> */}
+        <div>
+          <div>Fee price: {feePriceEthResponse?.result} ETH</div>
+          <div>
+            <label htmlFor='bid-amount'>Enter your bid amount:</label>
+            <br />
+            <input
+              id='bid-amount'
+              style={{ minWidth: 200 }}
+              type='text'
+              value={bidAmount != null ? bidAmount : defaultBidAmount}
+              onChange={(e) => changeBidAmount(e.target.value)}
+            />
+          </div>
+        </div>
 
         <div>
           <button
-            onClick={() => {
-              enterRaffle();
-            }}
+            disabled={pendingWrite}
+            onClick={() => enterRaffle(String(bidAmount))}
           >
             Enter raffle
           </button>
+
+          {address && address === raffleOwner ? (
+            <>
+              <button disabled={pendingWrite} onClick={() => pickWinner()}>
+                Pick winner
+              </button>
+            </>
+          ) : address && address !== raffleOwner ? (
+            <>
+              <button disabled={pendingWrite} onClick={() => requestDraw()}>
+                Request raffle draw
+              </button>
+            </>
+          ) : null}
         </div>
       </div>
     </div>
